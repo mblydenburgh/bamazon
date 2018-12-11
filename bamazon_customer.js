dbceb0f6fa12332aa780f76a0246dce98238e462
M Blydenburgh <mike.blydenburgh@gmail.com>
2018-12-10 20:58:14 -0500

    placing orders working

--- bamazon_customer.js
+++ bamazon_customer.js
@@ -8,10 +8,9 @@ const dbPassword = process.env.DB_PASSWORD;
 const numRegex = /^[0-9]+$/; //regular expression to accept only number input
 let sql;
 
-
 function createTable(headers) {
-    //try to take passed in headers to dynamically update table name from running initial .sql file
-    //and beautifying the header names
+    //try to take passed in headers to dynamically update names from running initial .sql file
+    //and beautifying the header names automatically
     let table = new Table({
         head: ["Item ID", "Name", "Department", "Price", "Quantity"],
         colWidths: [10, 45, 15, 10, 10]
@@ -20,104 +19,117 @@ function createTable(headers) {
 }
 
 function buyPromptID(data) {
-    //make array of valid item id's to validate against user input
-    let validIDs = data.map(item=>{return item.item_id});
-    console.log(validIDs);
-    inquirer.prompt([
-        {
-            message: 'Enter the product ID you wish to purchase:',
-            name: 'itemID'
-        }
-    ]).then(function (res) {
-        let userChoice = res.itemID;
-        // console.log(validIDs.indexOf(Number(userChoice)));
-        //check if user input passes number regex and if it is in list of valid IDs
-        if(numRegex.test(userChoice) && validIDs.indexOf(Number(userChoice)) >-1){
-            console.log(`valid id`);
-            buyPromptQty(userChoice,data);
-        }
-        else{
-            console.log(`Please enter a valid ID`);
-            buyPromptID(data);
-        }
-    });
+    return new Promise((resolve, reject) => {
+        //make array of valid item id's to validate against user input
+        let validIDs = data.map(item => { return item.item_id });
+        console.log(validIDs);
+        inquirer.prompt([
+            {
+                message: 'Enter the product ID you wish to purchase:',
+                name: 'itemID'
+            }
+        ]).then(function (res) {
+            let userChoice = res.itemID;
+            //check if user input passes number regex and if it is in list of valid IDs
+            if (numRegex.test(userChoice) && validIDs.indexOf(Number(userChoice)) > -1) {
+                console.log(`valid id`);
+                resolve(Number(userChoice));
+            }
+            else {
+                console.log(`Please enter a valid ID`);
+                buyPromptID(data);
+            }
+        });
+    })
+
 }
 
-function buyPromptQty(itemID,data) {
-    itemID = Number(itemID);
-    console.log(`buy prompt id:${itemID}`);
-    console.log(`selected item details:`);
-    console.log(data[itemID-1]);
-    const itemQty = Number(data[itemID-1].stock_quantity);
+function buyPromptQty(itemID, data) {
+    return new Promise((resolve, reject) => {
+        itemID = Number(itemID);
+        // console.log(`buy prompt id:${itemID}`);
+        // console.log(`selected item details:`);
+        // console.log(data[itemID - 1]);
+        const itemQty = Number(data[itemID - 1].stock_quantity);
+
+        console.log(`We have ${itemQty} left`);
+        inquirer.prompt([
+            {
+                message: 'Enter the quantity for your order:',
+                name: 'orderQty'
+            }
+        ]).then(function (res) {
+            let userChoice = res.orderQty;
+
+            //check if user input passes numRegex, and if enough items are in stock for order
+            if (numRegex.test(userChoice) && itemQty >= Number(userChoice)) {
+                console.log(`we have enough to cover the order`);
+                resolve(Number(userChoice));
+            }
+            else {
+                console.log(`Please enter a valid order number`);
+                buyPromptQty(itemID, data);
+            }
+        });
+    })
+
+}
+
+function calculateOrderTotal(itemID, itemQty,data) {
+    console.log(`finding order total for id:${itemID} & qty:${itemQty}`);
+    const unitPrice = data[itemID-1].price;
+    return (unitPrice*itemQty);
+}
+
+/* DATABASE CONNECTION CONFIG
+
+    for working in cloud9:
+        user: "mblydenburgh"
+        password: ""
     
-    console.log(`We have ${itemQty} left`); 
-    inquirer.prompt([
-        {
-            message:'Enter the quantity for your order:',
-            name: 'orderQty'
-        }
-    ]).then(function(res){
-        let userChoice = res.orderQty;
-
-        if(numRegex.test(userChoice) && itemQty >= Number(userChoice)){
-            console.log(`we have enough to cover the order`);
-            placeOrder(itemID,Number(userChoice));
-        }
-        else{
-            console.log(`Enter a valid quantity number`);
-            buyPromptQty(itemID,data);
-        }
-    });
-}
-
-
-function placeOrder(itemID,orderQty){
-    console.log(`placing order for itemID:${itemID}, qty:${orderQty}`);
-    dbConnection.query(`UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?`,[orderQty,itemID])
-    .then(function(err,res){
-        if(err) throw err;
-        console.log(res);
-        dbConnection.end();
-    });
-}
-
-/*
-for working in cloud9:
-    user: "mblydenburgh"
-    password: ""
-    
-for working on macbook locally:
-    user:"root"
-    password: dpPassword,
-    database:"bamazon_db"
+    for working on macbook locally:
+        user:"root"
+        password: dbPassword,
+        database:"bamazon_db"
 */
 
-//create connection to database using .env for secure connect
-const dbConnection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'mblydenburgh',
-    password: "",
-    database: 'c9'
-})
-    //once connected, query to select all data for displaying to product table
-    .then(function (connection) {
-        sql = `SELECT * FROM products`;
-        let data = connection.query(sql);
-        // connection.end();
-        return data;
+function connectToDB() {
+    //create connection to database using .env for secure connect
+    mysql.createConnection({
+        host: '127.0.0.1',
+        user: 'root',
+        password: dbPassword,
+        database: 'bamazon_db'
     })
-    .then(function (data) {
-        const headers = Object.keys(data[0]);
-        const table = createTable(headers);
-        data.forEach(row => {
-            let { item_id: id, product_name: name, department_name: department, price, stock_quantity: qty } = row;
-            // console.log(`*****PUSHING ROW*****`)
-            // console.log(id, name, department, price, qty);
-            table.push([id, name, department, `$${price}`, qty]);
-        });
-        //display table of products and prompt user to select which item
-        console.log(table.toString());
-        buyPromptID(data);
-    })
-    .catch(err => console.log(err)
-    );
+        //once connected, query to select all data for displaying to product table
+        .then(async function (connection) {
+            sql = `SELECT * FROM products`;
+            let data = await connection.query(sql);
+
+            const headers = Object.keys(data[0]);
+            const table = createTable(headers);
+            data.forEach(row => {
+                let { item_id: id, product_name: name, department_name: department, price, stock_quantity: qty } = row;
+                table.push([id, name, department, `$${price}`, qty]);
+            });
+            //display table of products and prompt user to select which item
+            console.log(table.toString());
+
+            //once product list is displayed, prompt user for id and quantity of their purchase order
+            let itemID = await buyPromptID(data);
+            let itemQty = await buyPromptQty(itemID, data);
+            let orderPrice = calculateOrderTotal(itemID, itemQty,data);
+            console.log(`Order Total: ${orderPrice}`);
+            
+            return connection.query(`UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?`, [itemQty, itemID]);
+        })
+        .then(async function (data) {
+            console.log(`Order placed successfully!`);
+            console.log(data.message);
+            setTimeout(connectToDB,3000);
+        })
+        .catch(err => console.log(err)
+        );
+}
+
+connectToDB();
